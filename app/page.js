961d062f916367b81e806bc7ff1c8b3d6d2e8331
joerundh <joerundh.p01@gmail.com>
2025-08-25 12:28:37 +0200

    changed detail in tailwind

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default async function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex flex-col items-center justify-between">
       <div className="w-[1000px] mx-auto flex flex-wrap gap-[20px]">
         {
           data?.map((post, index) => (
